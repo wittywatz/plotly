b53cc02686e5f28eb2c2bba2200366689944431e
wittywatz <wittywatz@gmail.com>
2022-09-11 14:45:35 -0400

    Import correction

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Flex, Box, useMediaQuery, useColorMode } from "@chakra-ui/react";
-import ThemeSelector from "./themeSelector";
-import LogoDark from "../assets/LogoDark.svg";
-import Logo from "../assets/logo.svg";
+import React from 'react';
+import { Flex, Box, useMediaQuery, useColorMode } from '@chakra-ui/react';
+import ThemeSelector from './ThemeSelector';
+import LogoDark from '../assets/LogoDark.svg';
+import Logo from '../assets/logo.svg';
 
 const NavBar = () => {
   const { colorMode } = useColorMode();
-  const [issmallerThan346px] = useMediaQuery("(max-width: 360px)");
+  const [issmallerThan346px] = useMediaQuery('(max-width: 360px)');
   return (
     <Flex
       flex={1}
       align="center"
-      justify={issmallerThan346px ? "center" : "space-between"}
-      direction={issmallerThan346px ? "column" : "row"}
+      justify={issmallerThan346px ? 'center' : 'space-between'}
+      direction={issmallerThan346px ? 'column' : 'row'}
     >
       <Box padding=" 0 1rem 0 1rem" marginBottom="1rem">
-        {colorMode == "dark" ? <LogoDark /> : <Logo />}
+        {colorMode == 'dark' ? <LogoDark /> : <Logo />}
       </Box>
       <Box padding=" 0 1rem 0 1rem" marginBottom="1rem">
         <ThemeSelector />
